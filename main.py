2a5c9929ce923fc0f57217a93b57fd273d65bd3c
Stephen Alex <stephenalex9182@gmail.com>
2026-08-26 16:24:10 +0530

    Update and rename ai.py to main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+API key loaded successfully.
+LLM initialized successfully.
+LangGraph compiled successfully.
+
+======================================================================
+🏠 REAL ESTATE MULTI-AGENT AI
+======================================================================
+
+Starting agents...
+
+
+======================================================================
+❌ EXECUTION ERROR
+======================================================================
+
+Error type:
+OpenAIAuthenticationError
+
+Error message:
+Error code: 401 - {'error': {'message': 'Incorrect API key provided: YOUR_OPE*******_KEY. You can find your API key at https://platform.openai.com/account/api-keys.', 'type': 'invalid_request_error', 'param': None, 'code': 'invalid_api_key'}}
